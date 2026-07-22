00847baa5ac12e24262785ed812c1d74d667a74f
jordandanielpenarrietasantilla-max <jordandanielpenarrietasantilla@gmail.com>
2026-07-22 17:13:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ INSTRUCCION_SISTEMA = (
 )
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-2.5-flash",
     system_instruction=INSTRUCCION_SISTEMA
 )
 
